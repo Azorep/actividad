d2f97109a5102cf7b84ffdfe7a3d74f4701f1897
Diego Peroza <diegoeleveth@gmail.com>
2025-04-03 22:08:07 +0200

    cambio de variables

--- src/practica1/notas.java
+++ src/practica1/notas.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class notas {
 	//declaramos la variables que nos hacen falta
-	double uf1, uf2, uf3;
+	double un1, un2, un3;
 	double acu1, acu2, acu3, def;
 	//utilizames ecaner para poder introducir datos
 	Scanner entrada = new Scanner(System.in);
@@ -17,16 +17,16 @@ public class notas {
 		
 		System.out.print("ingrese nota 1: ");
 		
-		uf1= entrada.nextDouble();
+		un1= entrada.nextDouble();
 
 		
 		
 		System.out.print("ingrese nota 2: ");
 		
-		uf2= entrada.nextDouble();
+		un2= entrada.nextDouble();
 		System.out.print("ingrese nota 3: ");
 		
-		uf3= entrada.nextDouble();
+		un3= entrada.nextDouble();
 		
 	
 	}
@@ -35,20 +35,20 @@ public class notas {
 	
 	public void comprobarcion(){
 		
-		if (uf1>10) {
+		if (un1>10) {
 			System.out.println(" nota1 mal introducida");
 			
 		}else {
 			System.out.println(" nota1 correcta");
 		}
 		
-		if (uf2>10) {
+		if (un2>10) {
 			System.out.println(" nota2 mal introducida");
 			
 		}else {
 			System.out.println(" nota2 correcta");
 		}
-		if (uf3>10) {
+		if (un3>10) {
 			System.out.println(" nota3 mal introducida");
 			
 		}else {
@@ -62,9 +62,9 @@ public class notas {
 	// metodo para calcular nota
 	
 	public void Calculonotas() {
-		acu1= uf1*0.35;
-		acu2 = uf2 * 0.35;
-		acu3 = uf3 * 0.30;
+		acu1= un1*0.35;
+		acu2 = un2 * 0.35;
+		acu3 = un3 * 0.30;
 		
 		def = acu1 + acu2+ acu3;
 		
@@ -73,9 +73,9 @@ public class notas {
 	public void Mostrar() {
 		//print ln lo que hace es que al terminar el print baja la linea
 		System.out.println(" notas introducidas son:");
-		System.out.println(" nota1 = " + uf1);
-		System.out.println(" nota2 = " + uf2);
-		System.out.println(" nota3 = " + uf3);
+		System.out.println(" nota1 = " + un1);
+		System.out.println(" nota2 = " + un2);
+		System.out.println(" nota3 = " + un3);
 		
 		System.out.println(" acumuado 1 = "+ acu1);
 		System.out.println(" acumuado 2 = "+ acu2);
